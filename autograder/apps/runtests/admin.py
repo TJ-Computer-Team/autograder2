30fd9a1a367c94f3aa3a28e82f09f16793fa1f8f
Samuel Chow <samuel.chow.45@gmail.com>
2025-10-04 22:09:38 -0400

    add rerun functionality

--- autograder/apps/runtests/admin.py
+++ autograder/apps/runtests/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Submission
+from .tasks import grade_submission_task
+
+
+@admin.action(description="Rerun submissions")
+def rerun_submissions(modeladmin, request, queryset):
+    for sub in queryset:
+        grade_submission_task.delay(sub.id)
 
 
 @admin.register(Submission)
@@ -20,9 +27,14 @@ class SubmissionAdmin(admin.ModelAdmin):
     readonly_fields = ("timestamp",)
 
     fieldsets = (
-        (None, {"fields": ("usr__display_name", "contest", "problem", "language", "code")}),
+        (
+            None,
+            {"fields": ("usr__display_name", "contest", "problem", "language", "code")},
+        ),
         (
             "Result Info",
             {"fields": ("verdict", "runtime", "timestamp", "insight")},
         ),
     )
+
+    actions = [rerun_submissions]
