ded3367c0b01bbe762435d46c6c17267adc799f5
Samuel Chow <samuel.chow.45@gmail.com>
2025-10-25 21:11:10 -0400

    make rerunning add a new sub instead

--- autograder/apps/runtests/admin.py
+++ autograder/apps/runtests/admin.py
@@ -5,8 +5,16 @@ from .tasks import grade_submission_task
 
 @admin.action(description="Rerun submissions")
 def rerun_submissions(modeladmin, request, queryset):
-    for sub in queryset:
-        grade_submission_task.delay(sub.id)
+    for old_sub in queryset:
+        new_sub = Submission.objects.create(
+            usr=old_sub.usr,
+            code=old_sub.code,
+            problem=old_sub.problem,
+            language=old_sub.language,
+            contest=old_sub.contest,
+        )
+        new_sub.save()
+        grade_submission_task.delay(new_sub.id)
 
 
 @admin.register(Submission)
